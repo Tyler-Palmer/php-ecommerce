7f151abd9509107c8ca525318de8c9912b0de79f
tyler <tyler.william.palmer@gmail.com>
2019-05-19 23:32:28 -0600

    dynamically generated navigation with php loops and mysql database

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,51 +1,30 @@
-<!-- Top Navbar -->
+<?php
+$sql = "SELECT * FROM categories WHERE parent = 0";
+$pquery = $db->query($sql);
+?>
+
 <nav class="navbar navbar-default navbar-fixed-top">
-            <div class="container">
-                <a href="index.php" class="navbar-brand">Shaunta's Boutique</a>
-                <ul class="nav justify-content-flex-start">
-                    <!-- Men's Clothes -->
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Men<span class="caret"></span></a>
+    <div class="container">
+        <div class="row justify-content-flex-start">
+            <a href="index.php" class="navbar-brand">Shaunta's Boutique</a>
+            <ul class="nav navbar-nav list-inline">
+                <?php while($parent = mysqli_fetch_assoc($pquery)) : ?>
+                    <?php 
+                        $parent_id = $parent['id']; 
+                        $sql2 = "SELECT * FROM categories WHERE parent = '$parent_id' ";
+                        $cquery = $db->query($sql2);
+                    ?>
+                    <!-- Menu Items -->
+                    <li class="dropdown nav-item">
+                        <a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown"><?php echo $parent['category']; ?><span class="caret"></span></a>
                         <ul class="dropdown-menu" role="menu">
-                            <li class="nav-item"><a href="#">Shirts</a></li>
-                            <li class="nav-item"><a href="#">Pants</a></li>
-                            <li class="nav-item"><a href="#">Shoes</a></li>
-                            <li class="nav-item"><a href="#">Accessories</a></li>
+                            <?php while($child = mysqli_fetch_assoc($cquery)) : ?>
+                                <li class="nav-item"><a href="#"><?php echo $child['category']; ?></a></li>
+                            <?php endwhile; ?>
                         </ul>
                     </li>
-
-                    <!-- Women's Clothes -->
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Women<span class="caret"></span></a>
-                        <ul class="dropdown-menu" role="menu">
-                            <li class="nav-item"><a href="#">Shirts</a></li>
-                            <li class="nav-item"><a href="#">Pants</a></li>
-                            <li class="nav-item"><a href="#">Shoes</a></li>
-                            <li class="nav-item"><a href="#">Accessories</a></li>
-                        </ul>
-                    </li>
-
-                    <!-- Boy's Clothes -->
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Boys<span class="caret"></span></a>
-                        <ul class="dropdown-menu" role="menu">
-                            <li class="nav-item"><a href="#">Shirts</a></li>
-                            <li class="nav-item"><a href="#">Pants</a></li>
-                            <li class="nav-item"><a href="#">Shoes</a></li>
-                            <li class="nav-item"><a href="#">Accessories</a></li>
-                        </ul>
-                    </li>
-
-                    <!-- Girl's Clothes -->
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Girls<span class="caret"></span></a>
-                        <ul class="dropdown-menu" role="menu">
-                            <li class="nav-item"><a href="#">Shirts</a></li>
-                            <li class="nav-item"><a href="#">Pants</a></li>
-                            <li class="nav-item"><a href="#">Shoes</a></li>
-                            <li class="nav-item"><a href="#">Accessories</a></li>
-                        </ul>
-                    </li>
-                </ul>
-            </div>
-        </nav>
+                <?php endwhile; ?>
+            </ul>
+        </div>
+    </div>
+</nav>
