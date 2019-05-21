36e5dffc280d15f56537b703f5249c72047ad8d5
tyler <tyler.william.palmer@gmail.com>
2019-05-20 23:33:40 -0600

    admin back end skeleton created

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+</div><br><br>
+    <footer class="container text-center" id="footer">&copy; Copyright 2019 Shaunta's Boutique</footer>
+        </div>
+    </div>
+    </body>
+</html>
---
+++ admin/includes/navigation.php
@@ -0,0 +1,17 @@
+<nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+        <div class="row justify-content-flex-start">
+            <a href="index.php" class="navbar-brand">Shaunta's Boutique Admin</a>
+            <ul class="nav navbar-nav list-inline">
+                <!-- Menu Items -->
+                <li class="nav-item"><a href="brands.php">Brands</a></li>
+                <!-- <li class="dropdown nav-item">
+                    <a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown"><?php echo $parent['category']; ?><span class="caret"></span></a>
+                    <ul class="dropdown-menu" role="menu">
+                        <li class="nav-item"><a href="#"></a></li>
+                    </ul>
+                </li> -->
+            </ul>
+        </div>
+    </div>
+</nav>
