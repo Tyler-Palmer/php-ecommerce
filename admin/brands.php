c0e07813b344d8db8e412ca8b563da6a674539e4
tyler <tyler.william.palmer@gmail.com>
2019-05-22 16:21:39 -0600

    admin edit functionality fully working

--- admin/brands.php
+++ admin/brands.php
@@ -8,6 +8,16 @@
     $results = $db->query($sql_get);
     $errors = array();
 
+    //Edit Brand
+
+    if(isset($_GET['edit']) && !empty($_GET['edit'])){
+        $edit_id = (int)$_GET['edit'];
+        $edit_id = sanitize($edit_id);
+        $sql_edit = "SELECT * FROM brand WHERE id = '$edit_id'";
+        $edit_result = $db->query($sql_edit);
+        $eBrand = mysqli_fetch_assoc($edit_result);
+    };
+
     //Delete Brand
     if(isset($_GET['delete']) && !empty($_GET['delete'])){
         $delete_id = (int)$_GET['delete'];
@@ -26,6 +36,9 @@
         }
         //Check if brand already exists in db
         $sql_check = "SELECT * FROM brand WHERE brand = '$brand'";
+        if(isset($_GET['edit'])){
+            $sql = "SELECT * FROM brand WHERE brand = '$brand' AND id != '$edit_id'";
+        }
         $result = $db->query($sql_check);
         $count = mysqli_num_rows($result);
         if($count > 0){
@@ -37,6 +50,9 @@
         }else{
             //Add brand to db
             $sql_add = "INSERT INTO brand (brand) VALUES ('$brand')";
+            if(isset($_GET['edit'])){
+                $sql_add = "UPDATE brand SET brand = '$brand' WHERE id = '$edit_id'";
+            }
             $db->query($sql_add);
             header('Location: brands.php');
         }
@@ -46,11 +62,24 @@
     <!-- Brand Form -->
     <div class="container">
         <div class="row justify-content-md-center">
-            <form class="form-inline" action="brands.php" method="post">
+            <form class="form-inline" action="brands.php<?=((isset($_GET['edit']))? '?edit='.$edit_id : ''); ?>" method="post">
                 <div class="form-group mb-2">
-                    <label for="brand">Add A Brand: </label>
-                    <input type="text" class="form-control" name="brand" id="brand" value="<?=((isset($_POST['brand']))?$_POST['brand']:''); ?>">
-                    <input type="submit" class="btn btn-md btn-success ml-1" name="add_submit" value="Add Brand">
+                    <?php 
+                        $brand_value = '';
+                        if(isset($_GET['edit'])){
+                        $brand_value = $eBrand['brand'];
+                        }else{
+                            if(isset($_POST['brand'])){
+                                $brand_value = sanitize($_POST['brand']);
+                            }
+                        } 
+                    ?>
+                    <label for="brand"><?= ((isset($_GET['edit']))? 'Edit':'Add A'); ?> Brand: </label>
+                    <input type="text" class="form-control" name="brand" id="brand" value="<?=$brand_value; ?>">
+                    <?php if(isset($_GET['edit'])): ?>
+                        <a href="brands.php" class="btn btn-primary">Cancel</a>
+                   <?php endif; ?>
+                    <input type="submit" class="btn btn-md btn-success ml-1" name="add_submit" value="<?=((isset($_GET['edit']))? 'Edit' : 'Add') ; ?>">
                 </div>
             </form>
         </div><hr>
